282582280bf7901031211352d50fa514e2433cea
kannanATexcelforte <kannan@excelforte.com>
2023-06-30 12:16:46 +0530

    30.06.2023-cfwaa
    completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './layouts/header/header.component';
 import { FooterComponent } from './layouts/footer/footer.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { environment } from 'src/environments/environment.prod';
 
 @NgModule({
   declarations: [
@@ -16,7 +18,9 @@ import { DashboardComponent } from './dashboard/dashboard.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
+    provideFirestore(() => getFirestore()),
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+    production: true,
+    firebaseConfig: {
+        apiKey: "AIzaSyD-mpW5uk7_9whtH2DMCPuD8B3kVMlPoRs",
+        authDomain: "ang-blog-1b14a.firebaseapp.com",
+        projectId: "ang-blog-1b14a",
+        storageBucket: "ang-blog-1b14a.appspot.com",
+        messagingSenderId: "360295675593",
+        appId: "1:360295675593:web:1d949633252e34f618bf18"
+    }
+}
