adebb26bfbb9a9c50891a4b923063485877bd58f
kannanATexcelforte <kannan@excelforte.com>
2023-07-12 13:06:55 +0530

    12.07.2023-atp
    completed

--- src/app/posts/new-post/new-post.component.ts
+++ src/app/posts/new-post/new-post.component.ts
@@ -7,4 +7,11 @@ import { Component } from '@angular/core';
 })
 export class NewPostComponent {
 
+  permalink: string = '';
+
+  onTitleChanged($event: any) {
+    const title = $event.target.value;
+    this.permalink = title.replace(/\s/g, '-');
+  }
+
 }
