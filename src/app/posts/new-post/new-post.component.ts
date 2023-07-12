87d0bd1892effe06f9ac77142384eacf707ed3cc
kannanATexcelforte <kannan@excelforte.com>
2023-07-12 16:45:07 +0530

    12.07.2023-spip
    completed

--- src/app/posts/new-post/new-post.component.ts
+++ src/app/posts/new-post/new-post.component.ts
@@ -8,10 +8,21 @@ import { Component } from '@angular/core';
 export class NewPostComponent {
 
   permalink: string = '';
+  imgSrc: any = './assets/placeholder-image.png';
+  selectedImg: any;
 
   onTitleChanged($event: any) {
     const title = $event.target.value;
     this.permalink = title.replace(/\s/g, '-');
+  }
+
+  showPreview($event: any) {
+    const reader = new FileReader();
+    reader.onload = (e:any) => {
+      this.imgSrc = e.target.result;
+    }
+    reader.readAsDataURL($event.target.files[0]);
+    this.selectedImg = $event.target.files[0];
   }
 
 }
