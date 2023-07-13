f9ca8efea9376d164f7d5fd26fd5fdd7636ac12a
kannanATexcelforte <kannan@excelforte.com>
2023-07-13 11:52:14 +0530

    13.07.2023-lpc
    completed

--- src/app/posts/new-post/new-post.component.ts
+++ src/app/posts/new-post/new-post.component.ts
@@ -1,15 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CategoriesService } from 'src/app/services/categories.service';
 
 @Component({
   selector: 'app-new-post',
   templateUrl: './new-post.component.html',
   styleUrls: ['./new-post.component.css']
 })
-export class NewPostComponent {
-
+export class NewPostComponent implements OnInit {
+  
   permalink: string = '';
   imgSrc: any = './assets/placeholder-image.png';
   selectedImg: any;
+
+  categories: Array<object> | any;
+
+  constructor(private categoryService: CategoriesService) { }
+
+  ngOnInit() {
+    this.categoryService.loadData().subscribe(val => {
+      this.categories = val;
+    });
+  }
 
   onTitleChanged($event: any) {
     const title = $event.target.value;
