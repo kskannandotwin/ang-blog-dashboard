1291cb2c19efdc9d5433c81fa6eab7923ed8c13c
kannanATexcelforte <kannan@excelforte.com>
2023-08-04 22:41:04 +0530

    04.08.2023-ctfv
    completed

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
-
+  onSubmit(formValue: any) {
+    console.log(formValue);
+  }
 }
