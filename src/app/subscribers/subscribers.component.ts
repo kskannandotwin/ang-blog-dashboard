a443a2b92aa28264eafc3a010b74b0e5c3be5c1e
kannanATexcelforte <kannan@excelforte.com>
2023-08-17 13:01:21 +0530

    17.08.2023-ctatf
    completed

---
+++ src/app/subscribers/subscribers.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { SubscribersService } from '../services/subscribers.service';
+
+@Component({
+  selector: 'app-subscribers',
+  templateUrl: './subscribers.component.html',
+  styleUrls: ['./subscribers.component.css']
+})
+export class SubscribersComponent {
+
+  subscribersArray: Array<object> | any;
+
+  constructor(private subService: SubscribersService) {}
+
+  ngOnInit() {
+    this.subService.loadData().subscribe(val => {
+      this.subscribersArray = val;
+    });
+  }
+
+}
